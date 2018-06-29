70c5f18915f9730d45b2ec80297cb66da17c95db
xuesemofa@126.com <xuesemofa@126.com>
2018-06-29 17:20:52 +0800

    首次提交

---
+++ src/main/java/com/ld/foundry/FoundryApplication.java
@@ -0,0 +1,12 @@
+package com.ld.foundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundryApplication.class, args);
+	}
+}
